a6674e1f41fd96b2cca40398efe6536f19a738fc
Giuseppe Averta <g.averta3@gmail.com>
2025-02-26 11:13:40 +0100

    add a few people

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Person:
         self.incantesimo = incantesimo
 
     def __str__(self):
-        return f"{self.nome} {self.cognome}"
+        return f"Person: {self.nome} {self.cognome} \n"
 
 class Student(Person):
     def __init__(self, nome, cognome, eta,
@@ -36,7 +36,7 @@ class Student(Person):
         self.animale = animale
 
     def __str__(self):
-        return f"Studente: {self.nome} - {self.cognome} - {self.casa} "
+        return f"Student: {self.nome} - {self.cognome} - {self.casa} \n "
 
     def __repr__(self):
         return f"Student(nome, cognome, eta, capelli, occhi, casa, animale)"
@@ -49,7 +49,8 @@ class Teacher(Person):
                  capelli, occhi, casa, materia, incantesimo="Non ancora definito"):
         super().__init__(nome, cognome, eta, capelli, occhi, casa, incantesimo)
         self.materia = materia
-
+    def __str__(self):
+        return f"Teacher: {self.nome} - {self.cognome} - {self.materia} \n "
 class Casa:
     def __init__(self, nome, studenti = [] ):
         self.nome = nome
@@ -69,15 +70,50 @@ class Casa:
 
         return mystr
 
-Harry = Person("Harry", "Potter", 11,
-               "castani", "azzurri", "Grifondoro")
-Ron = Student( "Ron", "Weasley", 11, "rossi", "castani",
-              "Grifondoro", "topo")
-Severus = Teacher("Severus", "Snape", 45, "neri", "neri",
-                  "Serpeverde", "Pozioni", "Sectumsempra")
-print(Harry)
-print(Ron)
-print(Severus)
+# Grifondoro
+Harry = Student(nome="Harry", cognome="Potter", eta=11, capelli="castani", occhi="azzurri", casa="Grifondoro", animale="civetta", incantesimo="Expecto Patronum")
+Hermione = Student(nome="Hermione", cognome="Granger", eta=11, capelli="castani", occhi="castani", casa="Grifondoro", animale="gatto", incantesimo="Wingardium Leviosa")
+Ron = Student(nome="Ron", cognome="Weasley", eta=11, capelli="rossi", occhi="azzurri", casa="Grifondoro", animale="topo")
+Neville = Student(nome="Neville", cognome="Paciock", eta=11, capelli="castani", occhi="castani", casa="Grifondoro", animale="rospo")
+Ginny = Student(nome="Ginny", cognome="Weasley", eta=10, capelli="rossi", occhi="castani", casa="Grifondoro", animale="gatto")
+Sirius = Person(nome="Sirius", cognome="Black", eta=36, capelli="neri", occhi="grigi", casa="Grifondoro") #Sirius non è ne studente ne professore ad Hogwarts
+Remus = Teacher(nome="Remus", cognome="Lupin", eta=36, capelli="castani", occhi="verdi", casa="Grifondoro", materia="Difesa contro le arti oscure")
+Minerva = Teacher(nome="Minerva", cognome="McGranitt", eta=70, capelli="neri", occhi="verdi", casa="Grifondoro", materia="Trasfigurazione", incantesimo="Trasfigurazione Animale")
+Albus = Teacher(nome="Albus", cognome="Silente", eta=115, capelli="argento", occhi="azzurri", casa="Grifondoro", materia="Preside")
+Rubeus = Person(nome="Rubeus", cognome="Hagrid", eta=60, capelli="neri", occhi="neri", casa="Grifondoro") #Rubeus non è ne studente ne professore ad Hogwarts
+James = Student(nome="James", cognome="Potter", eta=17, capelli="neri", occhi="castani", casa="Grifondoro", animale="cervo")
+Lily = Student(nome="Lily", cognome="Evans", eta=17, capelli="rosso", occhi="verdi", casa="Grifondoro", animale="nessuno")
+Fred = Student(nome = "Fred", cognome = "Weasley", eta = 16, capelli = "rossi", occhi = "castani", casa = "Grifondoro", animale="nessuno")
+George = Student(nome = "George", cognome = "Weasley", eta = 16, capelli = "rossi", occhi = "castani", casa = "Grifondoro", animale="nessuno")
+
+# Serpeverde
+Draco = Student(nome="Draco", cognome="Malfoy", eta=11, capelli="biondi", occhi="grigi", casa="Serpeverde", animale="nessuno")
+Severus = Teacher(nome="Severus", cognome="Snape", eta=45, capelli="neri", occhi="neri", casa="Serpeverde", materia="Pozioni", incantesimo="Sectumsempra")
+Horace = Teacher(nome="Horace", cognome="Lumacorno", eta=65, capelli="brizzolati", occhi="verdi", casa="Serpeverde", materia="Pozioni", )
+Bellatrix = Person(nome="Bellatrix", cognome="Lestrange", eta=47, capelli="neri", occhi="neri", casa="Serpeverde") #Bellatrix non è ne studente ne professore ad Hogwarts
+Lucius = Person(nome="Lucius", cognome="Malfoy", eta=42, capelli="biondi", occhi="grigi", casa="Serpeverde") #Lucius non è ne studente ne professore ad Hogwarts
+Narcissa = Person(nome="Narcissa", cognome="Malfoy", eta=41, capelli="biondi", occhi="azzurri", casa="Serpeverde") #Narcissa non è ne studente ne professore ad Hogwarts
+Pansy = Student(nome="Pansy", cognome="Parkinson", eta=12, capelli="neri", occhi="castani", casa="Serpeverde", animale="nessuno")
+Blaise = Student(nome = "Blaise", cognome = "Zabini", eta = 12, capelli = "neri", occhi = "castani", casa = "Serpeverde", animale="nessuno")
+
+# Corvonero
+Luna = Student(nome="Luna", cognome="Lovegood", eta=11, capelli="biondi", occhi="azzurri", casa="Corvonero", animale="nessuno")
+Cho = Student(nome="Cho", cognome="Chang", eta=12, capelli="neri", occhi="castani", casa="Corvonero", animale="nessuno")
+Gilderoy = Teacher(nome="Gilderoy", cognome="Allock", eta=33, capelli="biondi", occhi="azzurri", casa="Corvonero", materia="Difesa contro le Arti Oscure", incantesimo="Oblivion")
+Filius = Teacher(nome="Filius", cognome="Vitious", eta=150, capelli="bianchi", occhi="azzurri", casa="Corvonero", materia="Incantesimi", incantesimo="Wingardium Leviosa")
+Xenophilius = Person(nome="Xenophilius", cognome="Lovegood", eta=49, capelli="bianchi", occhi="azzurri", casa="Corvonero") #Xenophilius non è ne studente ne professore ad Hogwarts
+Padma = Student(nome="Padma", cognome="Patil", eta=13, capelli="neri", occhi="castani", casa="Corvonero", animale="nessuno")
+Michael = Student(nome = "Michael", cognome = "Corner", eta = 13, capelli = "neri", occhi = "castani", casa = "Corvonero", animale="nessuno")
+
+# Tassorosso
+Cedric = Student(nome="Cedric", cognome="Diggory", eta=16, capelli="castani", occhi="grigi", casa="Tassorosso", animale="nessuno")
+Pomona = Teacher(nome="Pomona", cognome="Sprout", eta=60, capelli="grigi", occhi="castani", casa="Tassorosso", materia="Erbologia")
+Hannah = Student(nome="Hannah", cognome="Abbott", eta=12, capelli="biondi", occhi="azzurri", casa="Tassorosso", animale="nessuno")
+Ernest = Student(nome="Ernest", cognome="Macmillan", eta=13, capelli="biondi", occhi="castani", casa="Tassorosso", animale="nessuno")
+Susan = Student(nome="Susan", cognome=" Bones", eta=12, capelli="rossi", occhi="verdi", casa="Tassorosso", animale="nessuno")
+Ted = Person(nome="Ted", cognome="Tonks", eta=24, capelli="castano", occhi="neri", casa="Tassorosso") #Ted non è ne studente ne professore ad Hogwarts
+
+print(Harry, Ron, Susan, Xenophilius, Remus)
 
 grifondoro = Casa("Grifodnoro", [Harry])
 print(grifondoro)
